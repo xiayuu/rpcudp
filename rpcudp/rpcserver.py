72e9eca951d0f817aa106012db1e5dea6d5ec2c3
Xia Yu <1013230155@qq.com>
2017-09-06 05:43:49 -0400

    refactor

--- rpcudp/rpcserver.py
+++ rpcudp/rpcserver.py
@@ -9,27 +9,28 @@ import os
 
 def rpccall(func):
     def _rpccall(self, dest, *args, **kw):
-        def _udpcall(conn, data, dest):
+        def _udpcall(data, dest):
             try:
+                conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 conn.sendto(data, dest)
-                self.debug("data send to dest")
                 with eventlet.Timeout(3, True):
-                    return conn.recvfrom(65500)
-            except Exception:
-                self.debug("rpc Exception")
+                    result, source = conn.recvfrom(65500)
+                    conn.close()
+                    if source != dest:
+                        self.debug("WARING:get msg from another host%s" % str(source))
+                    return result
+            except Exception, e:
+                self.debug("RPCEXCEPT:%s" % str(e))
                 return ""
 
-        self.debug("rpccall function %s" % func.__name__)
-        self.debug("rpccall dest: %s:%d" % dest)
+        self.debug("%s:c:call:%s:%s:%s" % (func.__name__, dest, str(args), str(kw)))
         msgid = sha1(os.urandom(32)).digest()
-        c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         data = msgid + msgpack.packb([func.__name__, args, kw])
-        self.pile.spawn(_udpcall, c, data, dest)
-        result, _ = self.pile.next()
-        self.debug("rcpcall returned")
-        c.close()
+        result = eventlet.spawn(_udpcall, data, dest).wait()
+        self.debug("%s:c:return:%s:%s" % (func.__name__, str(result), str(dest)))
         if msgid == result[0:20]:
             return msgpack.unpackb(result[20:], encoding='utf-8', use_list=False)
+
     return _rpccall
 
 def rpccall_n(timeout=3):
@@ -43,7 +44,8 @@ def rpccall_n(timeout=3):
                     c.sendto(data, dest)
                     res = None
                     with eventlet.Timeout(timeout, False):
-                        res, _ = c.recvfrom(65500)
+                        res, source = c.recvfrom(65500)
+                        self.debug("%s:c:return:%s:%s" % (func.__name__, str(res), str(source)))
                         if msgid != res[0:20]:
                             res = None
                         else:
@@ -51,38 +53,37 @@ def rpccall_n(timeout=3):
                                                 encoding='utf-8',
                                                 use_list=False)
                     c.close()
-                except Exception:
-                    self.debug("rpc Exception")
+                except Exception, e:
+                    self.debug("RPCEXCEPT:%s" % str(e))
                     res = None
 
                 return (res, dest)
+            pile = eventlet.greenpool.GreenPile()
             for dest in destlist:
-                self.pile.spawn(_udpcall, dest)
-            return self.pile
+                self.debug("%s:c:call:%s:%s:%s" % (func.__name__, dest, str(args), str(kw)))
+                pile.spawn(_udpcall, dest)
+            return pile
         return _rpccall_n
     return decorator
 
 class RPCServer(object):
     def __init__(self, DEBUG=False):
-        pool = eventlet.GreenPool()
-        self.pile = eventlet.GreenPile(pool)
         self.DEBUG = DEBUG
 
     def call_dispatch(self, datagram, dest, sock):
-        self.debug("call dispatch")
         msgid = datagram[0:20]
         data = msgpack.unpackb(datagram[20:], encoding='utf-8', use_list=False)
         funcname, args, kw = data
         f = getattr(self, "rpc_%s" % funcname, None)
         if f is not None and callable(f):
-            self.debug("call function %s" % funcname)
+            self.debug("%s:s:call:%s:%s" % (funcname, str(args), str(kw)))
             result = f(*args, **kw)
-            self.debug("finished call function %s" % funcname)
+            self.debug("%s:s:return:%s" % (funcname, str(result)))
             txdata = msgid + msgpack.packb(result)
-            self.debug("send data to %s:%d" % dest)
             sock.sendto(txdata, dest)
+            self.debug("%s:s:rpcres:%s" % (funcname, str(dest)))
         else:
-            self.debug("function %s is not callable " % funcname)
+            self.debug("%s:s:notexist" % funcname)
 
     def debug(self, msg):
         if self.DEBUG:
@@ -91,8 +92,6 @@ class RPCServer(object):
     def run(self, bindaddr):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(bindaddr)
-        self.debug("bind to addr %s:%d" % bindaddr)
         while True:
-            recvData, source = sock.recvfrom(1024)
-            self.debug("receive msg from %s:%d" % source)
+            recvData, source = sock.recvfrom(65500)
             eventlet.spawn_n(self.call_dispatch, recvData, source, sock)
